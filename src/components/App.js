bb4541a2178c25d61f7e5aa13393fc37791a9c11
buffalorob <peckham.rob@gmail.com>
2018-12-21 13:23:26 -0500

    now remders VidoDetail. onVideoSelect changes state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 
@@ -11,8 +12,8 @@ class App extends React.Component {
   }
 
   onVideoSelect = video => {
-    console.log('from the app', video);
-  }
+    this.setState({ selectedVideo: video });
+  };
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -28,6 +29,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
       </div>
     )
